a0fc0f6b9a9b22ff5afcccf7d0c72d185831a027
Simon Green <simonisaacgreen@icloud.com>
2019-01-21 06:48:21 +0000

    updating styling

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-var otherCheckbox = document.querySelector('input[value="other"]');
-var otherText = document.querySelector('input[id="otherValue"]');
+// Make text input appear when 'Other' checkbox is clicked
+const otherCheckbox = document.querySelector('input[value="other"]');
+const otherText = document.querySelector('input[id="otherValue"]');
 
 otherText.style.visibility = 'hidden';
 
 otherCheckbox.onchange = function() {
   if(otherCheckbox.checked) {
     otherText.style.visibility = 'visible';
+    otherText.placeholder = 'Please specify'
     otherText.value = '';
   } else {
     otherText.style.visibility = 'hidden';
